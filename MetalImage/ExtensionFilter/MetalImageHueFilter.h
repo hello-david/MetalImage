55dc1300c9873d54df02ae994ec2a189d42a11ac
david.dai <hello.david.me@gmail.com>
2019-03-25 14:46:26 +0800

    【Feature】删除无用代码，待实现Hue/ColorMatrix滤镜效果

---
+++ MetalImage/ExtensionFilter/MetalImageHueFilter.h
@@ -0,0 +1,16 @@
+//
+//  MetalImageHueFilter.h
+//  MetalImage
+//
+//  Created by David.Dai on 2019/3/25.
+//
+
+#import "MetalImageFilter.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MetalImageHueFilter : MetalImageFilter
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MetalImage/ExtensionFilter/MetalImageHueFilter.m
@@ -0,0 +1,12 @@
+//
+//  MetalImageHueFilter.m
+//  MetalImage
+//
+//  Created by David.Dai on 2019/3/25.
+//
+
+#import "MetalImageHueFilter.h"
+
+@implementation MetalImageHueFilter
+
+@end
